3fb88bd89b5afbc4db26ddba6eaa0673a2a1b004
sanma91 <mattiasanti1991@gmail.com>
2024-06-25 18:07:58 +0200

    Add method to create a grid, need to figure out how to fix sizes

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const container = document.querySelector("#container");
 
-for(let i = 0; i < 256; i++) {
-    const square = document.createElement("p");
-    square.textContent = i
-    container.appendChild(square);
-    square.setAttribute("style", "border: 2px solid red; height: 50px; width: 50px; margin: 0");
+function makeRows() {
+    for(let i = 0; i < 16; i++) {
+        const row = document.createElement("div");
+        container.appendChild(row);
+        row.setAttribute("style", "border: 2px solid red; height: 50px");
+        row.setAttribute("class", "row");
+        for(let j = 0; j < 16; j++) {
+            const square = document.createElement("div");
+            row.appendChild(square);
+            square.setAttribute("style", "border: 2px solid black; height: 50px; width: 50px")
+        }
+    }
 }
+
+makeRows()
+
+// ("style", "border: 2px solid red; height: 50px; width: 50px; margin: 0");
