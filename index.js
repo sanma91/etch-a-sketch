0a87fa7bd888ca4a1239024ead599693aa98c77b
sanma91 <mattiasanti1991@gmail.com>
2024-06-26 17:34:10 +0200

    Fix grid size

--- index.js
+++ index.js
@@ -4,17 +4,14 @@ function makeRows() {
     for(let i = 0; i < 16; i++) {
         const row = document.createElement("div");
         container.appendChild(row);
-        row.setAttribute("style", "border: 2px solid red; height: 50px");
+        row.setAttribute("style", "height: 50px")
         row.setAttribute("class", "row");
         for(let j = 0; j < 16; j++) {
             const square = document.createElement("div");
-            square.textContent = i;
             row.appendChild(square);
-            square.setAttribute("style", "border: 2px solid black; height: 100%; width: 100%")
+            square.setAttribute("style", "border: 2px solid black; height: 100%; width: 100%");
         }
     }
 }
 
 makeRows()
-
-// ("style", "border: 2px solid red; height: 50px; width: 50px; margin: 0");
