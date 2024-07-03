7bb5cc4adee754ea3eb88caec6d7ac18740e40c3
sanma91 <mattiasanti1991@gmail.com>
2024-07-03 23:03:28 +0200

    Fix the css: avoid hardcoding the square sizes in pixels by using flex-grow property

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 const container = document.querySelector("#container");
+container.setAttribute("style", "display: flex");
 
 const button = document.querySelector("button");
 
-function buttonPressed() {
+function clickButton() {
     button.addEventListener("click", () => {
-       let x = prompt("Choose the number of squares");
-       for(let i = 0; i < x; i++) {
-        const row = document.createElement("div");
-        container.appendChild(row);
-        row.setAttribute("style", "height: 50px")
-        row.setAttribute("class", "row");
-        for(let j = 0; j < x; j++) {
-            const square = document.createElement("div");
-            row.appendChild(square);
-            square.setAttribute("style", "border: 2px solid black; height: 100%; width: 100%");
-            square.addEventListener("mouseover", () => {
-                let rgb = [];
-                for(let k = 0; k < 3; k++) {
-                    rgb.push(Math.floor(Math.random() * 255))
+        x = prompt("Choose the number of squares");
+            for(let i = 0; i < x; i++) {
+                const row = document.createElement("div");
+                    row.setAttribute("style", "border: 1px solid gray; display: flex; flex-direction: column; flex-grow: 1");
+                    row.setAttribute("class", "row")
+                    container.appendChild(row);
+                        for(let j = 0; j < x; j++) {
+                            const square = document.createElement("div");
+                            square.setAttribute("style", "border: 1px solid gray; flex-grow: 1");
+                            square.setAttribute("class", "square");
+                            row.appendChild(square);
+                            square.addEventListener("mouseover", () => {
+                                let rgb = [];
+                                for(let k = 0; k < 3; k++) {
+                                    rgb.push(Math.floor(Math.random() * 255))
+                                }
+                                square.style.backgroundColor = 'rgb('+ rgb.join(',') +')'
+                            })
                 }
-                square.style.backgroundColor = 'rgb('+ rgb.join(',') +')'
-            })
-        }
-    }
-    });
+            }
+    })
 }
 
-buttonPressed()
+clickButton()
